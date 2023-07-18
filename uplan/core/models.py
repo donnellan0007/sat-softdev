994452725fa56f0969b01d577eb12351f9f442ee
Seamus Donnellan <donnellan0007@gmail.com>
2023-07-18 16:01:53 +1000

    ignore

--- uplan/core/models.py
+++ uplan/core/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     given_name = models.CharField(max_length=256, blank=True, null=True)
     family_name = models.CharField(max_length=256, blank=True, null=True)
     is_admin = models.BooleanField(default=False)
-    lesson_plans = models.ManyToManyField("LessonPlan", related_name="lesson_plan", blank=True, null=True)
+    lesson_plans = models.ManyToManyField("LessonPlan", related_name="lesson_plan", blank=True)
 
     def __str__(self):
         return f"{self.given_name} {self.family_name}"
